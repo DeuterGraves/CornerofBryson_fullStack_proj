59d08a4863c56e60208dd1331ec9ee823658c51b
DeuterGraves <carolinetgraves@gmail.com>
2018-12-13 22:16:49 +0000

    front and back end folders set up

---
+++ backend/brysonservice/src/main/java/com/example/brysonservice/BrysonserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.brysonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrysonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrysonserviceApplication.class, args);
+	}
+
+}
+
